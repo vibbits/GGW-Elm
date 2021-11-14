22ae77d85f537f3b91d9a774da9fd3311b33e299
Kevin De Pelseneer <kevin.depelseneer@gmail.com>
2021-11-14 19:57:13 +0100

    Implement feature read genbank files.
    * Added python files for reading the genbank files

--- server/app/router.py
+++ server/app/router.py
@@ -2,8 +2,8 @@
 
 from fastapi import APIRouter
 
-from app import auth, hello
+from app import auth, hellow_world
 
 router = APIRouter()
 router.include_router(auth.router)
-router.include_router(hello.router)
+router.include_router(hellow_world.router)
