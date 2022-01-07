5bf39b79db184a38a72d7373cd30b52ed7ddd9ac
Kevin De Pelseneer <kevin.depelseneer@gmail.com>
2022-01-07 14:09:41 +0100

    - Removed the CSS import that interfered with Elm-UI components. - Created an accordion pane

---
+++ client/src/Accordion.elm
@@ -0,0 +1,22 @@
+--| An accordion (hidable view) container
+module Accordion exposing (accordion, head, body)
+
+import Element exposing (..)
+
+type alias Head msg = Element msg
+type alias Body msg = Element msg
+
+type alias Display = Bool
+
+accordion : Head msg -> Body msg -> Display -> Element msg
+accordion accHead accBody display =
+    if display then 
+        column [ width fill] [ accHead, accBody ]
+    else
+        column [ width fill] [ accHead ]
+
+head : List (Element.Attribute msg) -> List (Element msg) -> Head msg
+head attrs = row ([width fill] ++ attrs)
+
+body : List (Element.Attribute msg) -> List (Element msg) -> Body msg
+body attrs = column ([width fill] ++ attrs)
