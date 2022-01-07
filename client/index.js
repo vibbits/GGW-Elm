383216f8535d63ef208223041ca689107cd81c03
Kevin De Pelseneer <82407142+kdp-cloud@users.noreply.github.com>
2022-01-07 15:32:53 +0100

    Update client/index.js
    Removing unused elm-accordion packages
    
    Co-authored-by: James Collier <26444312+MaybeJustJames@users.noreply.github.com>

--- client/index.js
+++ client/index.js
@@ -1,6 +1,3 @@
 import { Elm } from "./src/Main.elm";
 
-// require("elm-accordion/dist/elm-accordion.min.js");
-// require("elm-accordion/dist/elm-accordion.min.css");
-
 Elm.Main.init({ node: document.getElementById("elm") });
