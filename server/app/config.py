748e32962d80dca4531f45b28dad0c4fd09b2630
James Collier <james.collier@vib.be>
2022-01-10 20:02:36 +0100

    Create server CI configuration

--- server/app/config.py
+++ server/app/config.py
@@ -8,13 +8,13 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     "Global HTTP API server configuration"
 
-    DATABASE_URL: str = ""
+    DATABASE_URL: str = "sqlite:///ggw.sqlite?check_same_thread=false"
 
     API_SECRET: str = secrets.token_urlsafe(32)
     API_JWT_ALGORITHM: str = "HS256"
     API_TOKEN_EXPIRE: int = 7 * 24 * 60  # 7 days in minutes
 
-    AUTH_REDIRECT_URI: str
+    AUTH_REDIRECT_URI: str = "http://localhost:49999/oidc_login"
 
     MAX_TEMP_FILE_SIZE: int = 10 * 1024 * 1024
 
