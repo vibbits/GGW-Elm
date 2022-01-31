d4b5f5cf90fbfd1add97d61d54f58afb832e58ed
Kevin De Pelseneer <82407142+kdp-cloud@users.noreply.github.com>
2022-01-31 11:54:37 +0100

    Update server/app/vectors.py
    The console prints have been left out.
    
    Co-authored-by: James Collier <26444312+MaybeJustJames@users.noreply.github.com>

--- server/app/vectors.py
+++ server/app/vectors.py
@@ -16,10 +16,4 @@ def get_vectors(
     database: Session = Depends(deps.get_db),
     current_user: schemas.User = Depends(deps.get_current_user),
 ) -> List[Vector]:
-    v_list = crud.get_level0_for_user(database=database, user=current_user)
-    print("#" * 80)
-    print("Returning these vectors:")
-    [print(v) for v in v_list]
-    print("#" * 80)
-
-    return v_list
+    return crud.get_level0_for_user(database=database, user=current_user)
