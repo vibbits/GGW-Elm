eada4631bb22a9f456c526952c6a5b00023a28d3
Kevin De Pelseneer <kevin.depelseneer@gmail.com>
2022-02-07 15:58:21 +0100

    Solving remark 1 of PR

--- server/app/router.py
+++ server/app/router.py
@@ -3,11 +3,10 @@
 from fastapi import APIRouter
 
 from app import auth, submit, hello
-from app import vectors, backbones
+from app import vectors
 
 router = APIRouter()
 router.include_router(auth.router)
 router.include_router(submit.router)
 router.include_router(hello.router)
 router.include_router(vectors.router)
-router.include_router(backbones.router)
--- server/app/vectors.py
+++ server/app/vectors.py
@@ -15,3 +15,11 @@ def get_vectors(
     current_user: schemas.User = Depends(deps.get_current_user),
 ) -> List[Vector]:
     return crud.get_level0_for_user(database=database, user=current_user)
+
+
+@router.get("/vectors/backbones", response_model=List[schemas.Vector])
+def get_backbones(
+    database: Session = Depends(deps.get_db),
+    current_user: schemas.User = Depends(deps.get_current_user),
+) -> List[Vector]:
+    return crud.get_backbones_for_user(database=database, user=current_user)
