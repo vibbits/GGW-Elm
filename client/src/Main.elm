8a877a1e2661733505c27e28f18c5e44b1f1996e
Kevin De Pelseneer <82407142+kdp-cloud@users.noreply.github.com>
2022-02-21 08:33:51 +0100

    Update client/src/Main.elm
    Co-authored-by: James Collier <26444312+MaybeJustJames@users.noreply.github.com>

--- client/src/Main.elm
+++ client/src/Main.elm
@@ -389,7 +389,7 @@ view model =
         [ Element.layout
             [ Element.height Element.fill
             , inFront <| Notify.view CloseNotification model.notifications
-            , inFront <| navLinks
+            , inFront navLinks
             ]
             (row
                 [ Element.width Element.fill
