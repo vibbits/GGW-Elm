576cd5c31ffad65904eb733130847b2ee6cec88f
Kevin De Pelseneer <kevin.depelseneer@gmail.com>
2022-02-21 20:27:44 +0100

    Added new API end-points
    - Add a new vector
    
    - delete a vector by id

--- server/app/vectors.py
+++ server/app/vectors.py
@@ -31,3 +31,21 @@ def get_level1_constructs(
     current_user: schemas.User = Depends(deps.get_current_user),
 ) -> List[Vector]:
     return crud.get_level1_constructs_for_user(database=database, user=current_user)
+
+
+@router.post("/vectors", response_model=schemas.Vector)
+def add_level0_construct(
+    new_lvl0_vec: schemas.Vector,
+    database: Session = Depends(deps.get_db),
+    current_user: schemas.User = Depends(deps.get_current_user),
+) -> Vector:
+    return crud.add_vector(database=database, vector=new_lvl0_vec, user=current_user)
+
+
+@router.delete("/vectors/{vector_id}")
+def delete_vector(
+    vec_id: int,
+    database: Session = Depends(deps.get_db),
+    current_user: schemas.User = Depends(deps.get_current_user),
+):
+    vector = database.get(Vector, vec_id)
