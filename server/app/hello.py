d7925ae137b44e36eedaeee84db6c9c3108dc3fa
Kevin De Pelseneer <kevin.depelseneer@gmail.com>
2022-03-17 18:06:42 +0100

    Reformatted with Black

--- server/app/hello.py
+++ server/app/hello.py
@@ -8,7 +8,5 @@ router = APIRouter()
 
 @router.get("/hello", response_model=schemas.User)
 def hello(database: Session = Depends(deps.get_db)):
-    """Test function that returns the user name.
-
-    """
+    """Test function that returns the user name."""
     return crud.get_user(database, 1)
